2d1cdc10c8c7f7a5e50f8df3fc6549d94b4686d4
Bagus1508 <101942448+Bagus1508@users.noreply.github.com>
2025-01-28 02:58:43 +0700

    update login

--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -1,12 +1,39 @@
 <script setup>
+import { ref } from 'vue';
+import axios from 'axios';
+import { useRouter } from 'vue-router';
 
+const username = ref('');
+const password = ref('');
+const errorMessage = ref('');
+
+const router = useRouter();
+
+const login = async () => {
+  try {
+    // Kirim kredensial ke API
+    const response = await axios.post('http://localhost:3000/api/auth/login', {
+      username: username.value,
+      password: password.value
+    });
+
+    // Jika login berhasil, simpan token
+    const token = response.data.data.token;
+    localStorage.setItem('jwt', token); // Menyimpan token di localStorage
+
+    router.push('/'); // Redirect ke halaman dashboard atau yang diinginkan
+  } catch (error) {
+    // Tangani error jika login gagal
+    errorMessage.value = error.response ? error.response.data.messages : 'Terjadi kesalahan';
+  }
+};
 </script>
 
 <template>
 <body class="bg-[#5D5FEF] font-poppins">
   <section class="min-h-full">
     <div class="min-h-screen lg:mx-[300px] justify-center flex items-center">
-      <form action="#">
+      <form @submit.prevent="login">
         <div class="p-5 bg-white rounded-xl shadow-2xl">
           <h1 class="text-center text-[20px] font-semibold text-[#5B5B5B]">Silahkan Masuk</h1>
           <div class="flex my-[20px]">
@@ -15,11 +42,11 @@
           <div>
             <div class="mb-[10px]">
               <label for="username" class="text-[13px] text-[#5B5B5B] font-semibold mb-[10px]">Username</label>
-              <input id="username" type="text" class="bg-[#D9D9D9] w-full h-[40px] rounded-[10px]">
+              <input id="username" type="text" class="bg-[#D9D9D9] w-full h-[40px] rounded-[10px]" v-model="username">
             </div>
             <div>
               <label for="password" class="text-[13px] text-[#5B5B5B] font-semibold mb-[10px]">Password</label>
-              <input id="password" type="password" class="bg-[#D9D9D9] w-full h-[40px] rounded-[10px]">
+              <input id="password" type="password" class="bg-[#D9D9D9] w-full h-[40px] rounded-[10px]" v-model="password">
             </div>
           </div>
           <button type="submit" class="w-full bg-[#5D5FEF] p-[10px] text-white font-semibold rounded-[10px] mt-5 text-[13px]">Login</button>
